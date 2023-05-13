90336d4b69e8741c524ae3e6a2eaa2ea3f7e7570
Johnny Chiu <johnnychiu123@gmail.com>
2023-05-12 23:40:40 -0700

    exp-box changes

--- script.js
+++ script.js
@@ -4,16 +4,28 @@ const btnRight = document.querySelector('.btn--right');
 const contactMe = document.querySelector('.contact-me');
 const expItems = document.querySelectorAll('.exp-item');
 
+let currVisibleBox = undefined;
+
 expItems.forEach(item =>
   item.addEventListener('click', function (e) {
     e.preventDefault();
 
-    const exp = item.classList[1];
+    const contentBox = document.querySelector(`.content-${item.classList[1]}`);
+
+    if (currVisibleBox === contentBox) {
+      // Content box is visible, rehide the currrent box
+      contentBox.classList.add('hideup');
+      currVisibleBox = undefined;
+    } else {
+      if (currVisibleBox) currVisibleBox.classList.add('hideup');
+      contentBox.classList.remove('hideup');
+      currVisibleBox = contentBox;
+    }
   })
 );
 
-function hideAllExp() {
-  document
-    .querySelectorAll('.exp-box')
-    .forEach(box => box.classList.add('hideup'));
-}
+// function hideAllExp() {
+//   document
+//     .querySelectorAll('.exp-box')
+//     .forEach(box => box.classList.add('hideup'));
+// }
