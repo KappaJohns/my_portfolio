7d6d06c526438d915afd2808e25516d5186d32c6
Johnny Chiu <johnnychiu123@gmail.com>
2023-05-17 04:07:01 -0700

    allow contact me box appearing and carousel fix

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
 const btnLeft = document.querySelector('.btn--left');
 const btnRight = document.querySelector('.btn--right');
 
-const contactMe = document.querySelector('.contact-me');
+const contactMe = document.querySelectorAll('.contact');
 const expItems = document.querySelectorAll('.exp-item');
+const carouselBox = document.querySelector('.carousel-exp');
 
+const contactBox = document.querySelector('.contact-box');
+const contactBackground = document.querySelector('.contact-box--background');
+const formClose = document.querySelector('.contact-close');
+const formEle = document.getElementById('contact');
+
+// TODO
+// will need to set initGap dynamically
+const initGap = 48;
+
+let curItem = 0;
 let currVisibleBox = undefined;
 
-expItems.forEach(item =>
+const closeForm = () => contactBox.classList.add('hidden');
+
+// closing the form
+formEle.addEventListener('submit', function (e) {
+  e.preventDefault();
+  // TODO
+  // need to give alert or a fading textbox showing form successfully submitted
+  closeForm();
+});
+formClose.addEventListener('click', closeForm);
+contactBackground.addEventListener('click', function (e) {
+  if (e.target !== this) return;
+  closeForm();
+});
+
+contactMe.forEach(ele => {
+  ele.addEventListener('click', () => contactBox.classList.remove('hidden'));
+});
+
+expItems.forEach((item, i) => {
+  // createExpHoverStyle(item, i);
+  item.style.transform = `translateX(${i * (item.offsetWidth + initGap)}px)`;
+
   item.addEventListener('click', function (e) {
     e.preventDefault();
+    if (curItem + 2 < i) return;
 
     const contentBox = document.querySelector(`.content-${item.classList[1]}`);
 
@@ -18,14 +52,45 @@ expItems.forEach(item =>
       currVisibleBox = undefined;
     } else {
       if (currVisibleBox) currVisibleBox.classList.add('hideup');
-      contentBox.classList.remove('hideup');
+      setTimeout(() => contentBox.classList.remove('hideup'), 100);
       currVisibleBox = contentBox;
     }
-  })
-);
+  });
+});
 
-// function hideAllExp() {
-//   document
-//     .querySelectorAll('.exp-box')
-//     .forEach(box => box.classList.add('hideup'));
-// }
+btnLeft.addEventListener('click', () => moveItems('left'));
+btnRight.addEventListener('click', () => moveItems('right'));
+
+function moveItems(dir) {
+  if (dir === 'left' && curItem === 0) return;
+  if (dir === 'right' && curItem === 5) return;
+
+  const itemWidth = expItems[0].offsetWidth;
+  const dist = itemWidth + initGap;
+
+  if (dir === 'left') curItem--;
+  else curItem++;
+
+  expItems.forEach(
+    (item, i) =>
+      (item.style.transform = `translateX(${
+        i * (item.offsetWidth + initGap) + dist * -curItem
+      }px`)
+  );
+}
+
+function createExpHoverStyle(item, pos) {
+  const css = `.exp-item:hover {
+    cursor: pointer;
+    transform: translate(${pos * (item.offsetWidth + initGap)}px,  -1.2rem);
+    box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.04);
+  }`;
+
+  const style = document.createElement('style');
+  style.appendChild(document.createTextNode(css));
+  // style.styleSheets.cssText = css;
+
+  console.log();
+}
+
+console.log(document.styleSheets);
