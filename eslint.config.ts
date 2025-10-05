c67a0b8edc7012c529a19d43d903c5024a779d03
Johnny Chiu <johnnychiu123@gmail.com>
2025-10-04 22:10:06 -0700

    init

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,18 +1,19 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
+        ecmaFeatures: { tsx: true },
         sourceType: 'module',
       },
     },
@@ -23,6 +24,7 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      ...tseslint.configs.recommended,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
@@ -30,4 +32,4 @@ export default [
       ],
     },
   },
-]
+];
---
+++ src/Navigation.tsx
@@ -0,0 +1,7 @@
+import { Anchor, Box } from '@mantine/core';
+import classes from './Navigation.css';
+
+const links = [{ link: '/projects', label: 'Projects' }];
+export function Navigation() {
+  return <div>Hello</div>;
+}
